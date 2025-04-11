2c7b166107ff8dbf57a78e29a2bff9cd0e0b49a2
Arslan Yousaf <devtechofficial90@gmail.com>
2025-04-11 13:43:03 +0500

    Add forgot password widget and update login/register buttons in SignInScreen

--- lib/screens/sigin_in/widgets/sigin_in_widget.dart
+++ lib/screens/sigin_in/widgets/sigin_in_widget.dart
@@ -119,3 +119,57 @@ Widget buildTextField(String hintText, String textType, String iconName) {
     ),
   );
 }
+
+Widget forgotPassword() {
+  return Container(
+    margin: EdgeInsets.only(left: 25.h),
+    width: 260.w,
+    height: 44.h,
+    child: GestureDetector(
+      onTap: () {},
+      child: Text(
+        "Forgot password",
+        style: TextStyle(
+          color: Colors.black,
+          decoration: TextDecoration.underline,
+          fontSize: 12.sp,
+          decorationColor: Colors.blue,
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildLoginAndRegButton(String buttonName, String buttonType) {
+  return Container(
+    width: 325.w,
+    height: 50.h,
+    margin: EdgeInsets.only(
+      left: 25.w,
+      right: 25.w,
+      top: buttonType == "login" ? 40.h : 20.h,
+    ),
+    decoration: BoxDecoration(
+      color: Colors.blue,
+      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+      boxShadow: [
+        BoxShadow(
+          spreadRadius: 1,
+          blurRadius: 2,
+          offset: Offset(0, 1),
+          color: Colors.grey.withOpacity(0.1),
+        ),
+      ],
+    ),
+    child: Center(
+      child: Text(
+        buttonName,
+        style: TextStyle(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),
+      ),
+    ),
+  );
+}
