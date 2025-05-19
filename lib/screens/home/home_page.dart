c6ea93994f87ce0f27f700abd194305f1ae74424
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-19 12:02:58 +0500

    Add HomePage and HomePageAppBar components with custom app bar implementation

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:code_academy_app/screens/home/widgets/home_page_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: HomePageAppBar());
+  }
+}
