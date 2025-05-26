fe5121afd2cd5f0f8d1ef44bbdffd957b9c1df25
Arslan Yousaf <devtechofficial90@gmail.com>
2025-05-26 14:55:35 +0500

    Enhance HomePageSearchBox with improved TextField styling and hint text color

--- lib/screens/home/widgets/home_page_search_box.dart
+++ lib/screens/home/widgets/home_page_search_box.dart
@@ -25,7 +25,56 @@ class HomePageSearchBox extends StatelessWidget {
                 height: 16.w,
                 child: Image.asset("assets/icons/search.png"),
               ),
+              Container(
+                width: 240.w,
+                height: 40.h,
+                child: TextField(
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 11.h,
+                      horizontal: 10.w,
+                    ),
+                    hintText: "Search your courses",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    hintStyle: TextStyle(
+                      color: AppColors.primarySecondaryElementText,
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontFamily: "Avenir",
+                    fontWeight: FontWeight.normal,
+                    fontSize: 14.sp,
+                  ),
+                  autocorrect: false,
+                  obscureText: false,
+                ),
+              ),
             ],
+          ),
+        ),
+        GestureDetector(
+          child: Container(
+            width: 40.w,
+            height: 40.h,
+            decoration: BoxDecoration(
+              color: AppColors.primaryBackground,
+              borderRadius: BorderRadius.circular(15.r),
+              border: Border.all(color: AppColors.primaryFourElementText),
+            ),
+            child: Center(child: Image.asset("assets/icons/filter.png")),
           ),
         ),
       ],
